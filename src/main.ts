e15d53bdd6ddb660d2148d7d2f2cb65014874634
lamhungypl <lamhungypl@gmail.com>
2025-04-17 01:02:59 +0700

    stuff

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+const port = process.env.PORT || 5500
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.listen(5500);
+  await app.listen(port,()=>{
+    console.log(`Example app listening on port ${port}`)
+  });
 }
 bootstrap();
